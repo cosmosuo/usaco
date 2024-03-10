dd34026aa24ab23257338139d22d4bba499b7e91
cosmosu <livytence@gmail.com>
2024-03-10 15:57:35 -0400

    ride usaco

---
+++ training/ch1/ride.py
@@ -0,0 +1,28 @@
+"""
+ID: livyten1
+LANG: PYTHON2
+TASK: ride
+"""
+
+with open("ride.in","r")as fIn, open("ride.out", "w")as fOut:
+    lines = fIn.readlines()
+
+    cometName = lines[0].strip()
+    groupName = lines[1].strip()
+
+    cometCode = 1
+    for c in cometName:
+        cometCode = cometCode * (ord(c)-64)
+
+    cometCode = cometCode % 47
+
+    groupCode = 1
+    for c in groupName:
+        groupCode = groupCode * (ord(c)-64)
+
+    groupCode = groupCode % 47
+
+    if groupCode == cometCode : 
+        fOut.write("GO\n")
+    else:
+        fOut.write("STAY\n")

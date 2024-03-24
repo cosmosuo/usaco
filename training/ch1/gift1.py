ff12cdf936f817a1d18b8a26e2090b6c352e0399
cosmosu <livytence@gmail.com>
2024-03-24 16:17:06 -0400

    finished code

---
+++ training/ch1/gift1.py
@@ -0,0 +1,42 @@
+"""
+ID: livyten1
+LANG: PYTHON2
+TASK: gift1
+"""
+def getRemainingAfterGiving(money, peopleNum):
+      return money % peopleNum
+
+def getGivingForEachPeople(money, peopleNum):
+      return money// peopleNum
+
+with open("gift1.in", "r") as fIn:
+        lines = fIn.read().splitlines()
+
+np = int(lines[0])
+people = {}
+
+for i in range(1,np+1):
+    name = lines[i]
+    people[name] = 0
+
+curLine = np + 1
+while curLine < len(lines):
+      giver = lines[curLine]
+      curLine += 1
+      money, peopleNum = map(int, lines[curLine].split())
+      if peopleNum == 0:
+            curLine += 1
+            continue
+      people[giver] -= money
+      people[giver] = getRemainingAfterGiving(money, peopleNum)
+      curLine += 1
+      for i in range(peopleNum):
+            receiver = lines[curLine]
+            people[receiver] += getGivingForEachPeople(money, peopleNum)
+            curLine += 1
+
+print(people)
+
+
+
+      
